b44f84f330af44202d707853282ea0f00da390f6
Chiam <chiam_zhang@foxmail.com>
2024-09-16 21:20:39 +0800

    Site updated: 2024-09-16 21:20:25

--- js/funnyTitle.js
+++ js/funnyTitle.js
@@ -4,12 +4,12 @@
  document.addEventListener('visibilitychange', function () {
      if (document.hidden) {
          $('[rel="icon"]').attr('href', "/img/trhx2.png");
-         document.title = 'ヽ(●-`Д´-)ノ你丑你就走！';
+         document.title = 'ヽ(●-`Д´-)ノ welecome！';
          clearTimeout(titleTime);
      }
      else {
          $('[rel="icon"]').attr('href', "/img/trhx2.png");
-         document.title = 'ヾ(Ő∀Ő3)ノ你帅就回来！' + OriginTitle;
+         document.title = 'ヾ(Ő∀Ő3)ノPlease Go Back！' + OriginTitle;
          titleTime = setTimeout(function () {
              document.title = OriginTitle;
          }, 2000);
